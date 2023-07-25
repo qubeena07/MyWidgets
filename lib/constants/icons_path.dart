81a2dc39f7e1658fa4531a544264959e32a5f20c
Dipika <qubeena7@gmail.com>
2023-07-25 12:49:49 +0545

    compressed the image

--- lib/constants/icons_path.dart
+++ lib/constants/icons_path.dart
@@ -1,7 +1,5 @@
 class AssetIcons {
   static const String location = 'assets/svg';
-  static const String walkingManIcon = '$location/walkingMan.svg';
-  static const String checkInIcon = '$location/checkIn.svg';
   static const String checkOutIcon = '$location/checkOut.svg';
   static const String settingsIcon = '$location/settings.svg';
   static const String propertyHouseIcon = '$location/propertyHouse.svg';
